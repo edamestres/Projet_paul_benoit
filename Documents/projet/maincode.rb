6c0e0b0529f80fc133fe6b057211bc88194cd3d4
Pol <test@test.fr>
2016-03-22 18:40:38 +0100

    first add project

---
+++ Documents/projet/maincode.rb
@@ -0,0 +1,44 @@
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width"/>
+		<link rel="stylesheet" media="screen and (max-width: 800px)" href="style.css">
+	</head>
+		<body>
+
+					<header>
+						<div id="carrousel">
+							<ul>
+								<li><img src="" /></li>
+								<li><img src="" /></li>
+								<li><img src="" /></li>
+								<li><img src="" /></li>
+								<li><img src="" /></li>
+						</div>
+					</header>
+			<nav>
+			
+			<ul id="nav">
+   				<li id="nav-acceuil"><a href="#">Accueil</a></li>
+   				<li id="nav-mineral"><a href="#">Mineral</a></li>
+   				<li id="nav-vegetal"><a href="#">Vegetal</a></li>
+   				<li id="nav-accesoires"><a href="#">Accesoires</a></li>
+   				<li id="nav-nosservices"><a href="#">Nos Services</a></li>
+   				<li id="nav-conception"><a href="#">Conception</a></li>
+   				<li id="nav-quisommesnous"><a href="#">Qui Sommes nous?</a></li>
+   				<li id="nav-contact"><a href="#">Contact</a></li>
+			</ul>
+			<section>
+
+           </nav>
+		<image src="/ecole/logo.jpg">
+		//correspond au logo en haut a gauche
+		<a href=# class=pull-left><img src=ecole/logo.jpg></a> 
+		<nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"></a>
+            </div>
+
+		</body>
+</html>
